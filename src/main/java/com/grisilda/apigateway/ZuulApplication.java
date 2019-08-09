0b23e254e5d0e2791b22ddcbfd37cf32573ffc00
Grisilda <46270641+Grisilda@users.noreply.github.com>
2019-08-09 06:59:24 +0200

    Update ZuulApplication.java

--- src/main/java/com/grisilda/apigateway/ZuulApplication.java
+++ src/main/java/com/grisilda/apigateway/ZuulApplication.java
@@ -2,9 +2,9 @@ package com.grisilda.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+// import org.springframework.cloud.context.config.annotation.RefreshScope;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+// import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
